bd53e64dc1bba20baa6d8ea545e11468eb239f79
Shaheer-codex <shaheergaming766@gmail.com>
2026-05-13 19:32:25 +0500

    Add files via upload

---
+++ ACP1.py
@@ -0,0 +1,8 @@
+import math
+
+angle = float(input("Enter angle in degrees: "))
+rad = math.radians(angle)
+
+print(f"sin: {math.sin(rad):.4f}")
+print(f"cos: {math.cos(rad):.4f}")
+print(f"tan: {math.tan(rad):.4f}")
